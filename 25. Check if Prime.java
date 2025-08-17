38d7708c795804397c136f4748d59d083b1eb6aa
Dakshin Priya T S <tsdakshinpriya@gmail.com>
2025-08-17 18:47:28 +0530

    Update and rename Check if Prime.java to 25. Check if Prime.java

--- 25. Check if Prime.java
+++ 25. Check if Prime.java
@@ -19,7 +19,10 @@ public class PrimeCheck {
         if (n <= 1) return false;  // 0 and 1 are not prime
 
         // Check divisors from 2 to sqrt(n)
-        for (int i = 2; i * i <= n; i++) {
+        double sqrtN = Math.sqrt(n);
+        // for (int i = 2; i * i <= n; i++) 
+        for (int i = 2; i <= sqrtN; i++) 
+        {
             if (n % i == 0) return false;  // divisible → not prime
         }
         return true;  // no divisors → prime
